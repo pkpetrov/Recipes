e49b4099c3b93062646444f9a15dc7b2240070d3
Petar <pkpetrov1991@gmail.com>
2018-07-08 13:51:55 +0300

    - Added nslog

--- RecipeApp/ViewController.m
+++ RecipeApp/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"This is the first controller");
+    NSLog(@"This is second try");
 }
 
 

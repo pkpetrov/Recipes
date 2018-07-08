a5f525d70ffaefe373a6d8d7d3e073e537ddbef5
Petar <pkpetrov1991@gmail.com>
2018-07-08 13:56:41 +0300

    -Added final nslog

--- RecipeApp/ViewController.m
+++ RecipeApp/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"This is the first controller");
     NSLog(@"This is second try");
+    NSLog(@"This is impossible");
 }
 
 
